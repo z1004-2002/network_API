a390a8cb0dc7a76ab5611e25ff89cb7b8faa81d0
z1004-2002 <zogningfranc@gmail.com>
2023-03-15 10:09:05 +0100

    first

---
+++ src/main/java/com/vetrix/network_API/NetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.vetrix.network_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetrix/network_API/NetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetrix.network_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
