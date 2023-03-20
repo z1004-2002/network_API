f6499b981279d45e4e5cae44b3c6658213fe0306
z1004-2002 <zogningfranc@gmail.com>
2023-03-20 18:36:10 +0100

    first

--- src/main/java/com/vetrix/network_API/NetworkApiApplication.java
+++ src/main/java/com/vetrix/network_API/NetworkApiApplication.java
@@ -2,6 +2,9 @@ package com.vetrix.network_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class NetworkApiApplication {
@@ -9,5 +12,6 @@ public class NetworkApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NetworkApiApplication.class, args);
 	}
+
 
 }
